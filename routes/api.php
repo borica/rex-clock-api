144d6b799e75efc3ab83ec838c4dda1863ed6faf
Tiago Borica <tiago.borica@gmail.com>
2023-07-30 12:35:48 -0700

    - Creating api healthCheck route to validate if is listening to request at http://localhost/api/public

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/public', function () {
+    return response()->json('Rex-Clock-Api is listening for requests.', Response::HTTP_OK);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
