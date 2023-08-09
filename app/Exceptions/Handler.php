7f19bc3bc9db1e6d21f535e5e467b8463c77c1e7
Tiago Borica <tiago.borica@gmail.com>
2023-08-08 17:22:52 -0700

    - rm unused import;

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,7 +5,6 @@ namespace App\Exceptions;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
